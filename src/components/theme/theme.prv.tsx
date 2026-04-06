cac55df400fd052e42cd350fae3bbb1d3c61a76a
jeremiah-ndiritu <ndiritujeremiah24@gmail.com>
2026-04-06 21:29:32 +0300

    try fixxing theme toggle by adding a script in `index.html`

--- src/components/theme/theme.prv.tsx
+++ src/components/theme/theme.prv.tsx
@@ -19,6 +19,10 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
     localStorage.setItem("endea-theme", theme);
   }, [theme]);
 
+  /**
+   * Toggles the site theme
+   * @returns {void}
+   */
   const toggleTheme = () =>
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
 
